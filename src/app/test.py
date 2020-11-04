d0f91783bea33fab63c9a6ce9d7cfd47f0b7f148
dorjecurreli <dorjecurreli@gmail.com>
2020-11-04 21:31:30 +0100

    Added several credentials

--- src/app/test.py
+++ src/app/test.py
@@ -1,50 +1,11 @@
 #!/usr/bin/env python3
-import hashlib
 
 
 
-#change_fruits = input('Neu fruit name: ')
+messages = []
+for x in range(0,5):
+    string = input("enter string")
 
+    messages.append(string)
 
-#fruit = {
-#    0: {
-#        "banana": 1.00,
-#        "apple": 1.53,
-#        "kiwi": 2.00,
-#        "avocado": 3.23,
-#        "mango": 2.33,
-#        "pineapple": 1.44,
-#        "strawberries": 1.95,
-#        "melon": 2.34,
-#        "grapes": 0.98
-#    },
-#    1: {
-#        "banana": 1.00,
-#        "apple": 1.53,
-#        "kiwi": 2.00,
-#        "avocado": 3.23,
-#        "mango": 2.33,
-#        "pineapple": 1.44,
-#        "strawberries": 1.95,
-#        "melon": 2.34,
-#        "grapes": 0.7
-#    }
-#}
-
-
-#for k,v in fruit.items():
-#    fruit[1]['banana'] = change_fruits
-#    print(fruit)
-
-
-
-
-
-str = hashlib.sha256(b'hash this text')
-text_hashed = str.hexdigest()
-print(text_hashed)
-
-
-var = input('Input string: ').encode('utf-8')
-hashed_var = hashlib.sha256(var).hexdigest()
-print(hashed_var)
+print(messages)
